57f4acfa694eb113ae92142d1810f104e814b19f
Zuzia Zuzewicz <zuzannazuzewicz@MacBook-Air-Zuzia.local>
2018-01-09 22:56:40 +0100

    Fixed a problem with image not displaying

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var http = require('http');
 var fs = require('fs');
 
-fs.readFile('./index.html', 'utf-8', function(err, data){
-	console.log(data);
 
-	var server = http.createServer();
-
-	server.on('request', function(request, response) {
-		response.setHeader("Content-Type", "text/html; charset=utf-8");
-		if (request.method === 'GET' && request.url === '/') {
+var server = http.createServer();
+server.on('request', function(request, response) {
+	response.setHeader("Content-Type", "text/html; charset=utf-8");
+	if (request.method === 'GET' && request.url === '/') {
+		fs.readFile('./index.html', 'utf-8', function(err, data) {
+			if (err) throw err;
 			response.write(data);
 			response.end();
-		} else {
-			response.statusCode = 404;
-			response.write('<img src="404.jpg">');
-			response.end();
-		}
-	});
-	server.listen(9000); 
-	// Skąd wiemy, co wpisać w nawiasie?
+		});	
+	} else {
+			fs.readFile('./404.jpg', function(err, data) {
+			response.writeHead(200, {"Content-Type": "image/jpg"});
+			response.end(data);
+		});
+	}
 });
+server.listen(9000); 
+
